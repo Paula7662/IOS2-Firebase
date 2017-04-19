803b933bde162a978a6b4314906709347b9e4bee
Paula sausor <paulavalencianosausor@Paulas-Mac.local>
2017-04-19 20:59:46 +0200

    Initial Commit

---
+++ Actividad2-ios-firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Actividad2-ios-firebase
+//
+//  Created by Paula Valenciano sausor on 19/04/2017.
+//  Copyright © 2017 u-tad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
